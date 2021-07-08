e56719c945b92a48b605139352efef7fb6157c13
jacky6016 <chlin6016@gmail.com>
2021-07-07 22:16:37 -0700

    Use component props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 // The root App component
-// JSX must return one parent component (or wrapped in <>...</>)
 import Header from './components/Header'
 
-// Using a functional component
-// const App = () => {
-//   return (
-//     <div className='container'>
-//         <Header />
-//     </div>
-//   )
-// }
-
-// if using class component
-import React from 'react'
-class App extends React.Component {
-  render() {
-    return <h1>Hello from a class</h1>
-  }
+const App = () => {
+  return (
+    // passing a prop
+    <div className='container'>
+        <Header title='Hello' />
+    </div>
+  )
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 // using the `rafce` boilerplate from the ES7 React extension (React import deleted)
-const Header = () => {
+// props can be de-structured into parameters {title}
+const Header = (props) => {
     return (
         <header>
-            <h1>Hello From React</h1>
+            <h1>{props.title}</h1>
         </header>
     )
+}
+
+// setting default prop
+Header.defaultProps = {
+    title: 'Task Tracker'
 }
 
 export default Header
