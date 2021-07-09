dd9d8f04e1e4f157fb8aaaf1abb0990e00f9c6f8
jacky6016 <chlin6016@gmail.com>
2021-07-09 10:59:09 -0700

    Add styling 1. Use a dedicated CSS file: src/index.css 2. CSS in JS: pass objects to the style attribute 'style'

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types'
 
 const Header = (props) => {
     return (
-        <header>
-            <h1>{props.title}</h1>
+        <header className='header'>
+            <h1 style={headingStyle}>{props.title}</h1>
+            <h1 style={{color: 'blue'}}>Demonstrating style</h1>
+            <button className='btn'>Add</button>
         </header>
     )
 }
@@ -15,8 +17,14 @@ Header.defaultProps = {
 }
 
 // we will receive warning if prop type isn't same as specified
-Header.PropTypes = {
+Header.propTypes = {
     title: PropTypes.string.isRequired
+}
+
+// CSS in JS
+const headingStyle = {
+    color: 'red', 
+    backgroundColor: 'black'
 }
 
 export default Header
